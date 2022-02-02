026c641cba9f51c3a5e86b030c3546569a141a60
Paulo Menezes <paulormj@pbh.gov.br>
2022-02-02 12:23:31 -0300

    Discord da alura

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ export default function PaginaInicial() {
             as="form"
             onSubmit={function (infosDoEvento){
                 infosDoEvento.preventDefault();
-                roteamento.push('/chat');
+                roteamento.push( `/chat?username=${username}`);
               }
             
             }
